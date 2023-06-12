73d334568bff76423102caf1448ece7e394ef65c
Carter Garcia <bikabattles@gmail.com>
2023-06-12 13:41:15 -0400

    Added Javascript Listeners

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var fadeElement = document.querySelector(".fade-in");
+    fadeElement.classList.add("fade-in-visible");
+});
+
+
+window.addEventListener("scroll", function() {
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
