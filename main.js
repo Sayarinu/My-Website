da80c36af9010a26be84c96facd332b980868ebd
Carter Garcia <bikabattles@gmail.com>
2023-06-12 16:43:41 -0400

    Landing Page Complete

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     fadeElement.classList.add("fade-in-visible");
 });
 
-
+/*
 window.addEventListener("scroll", function() {
     let header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+*/
